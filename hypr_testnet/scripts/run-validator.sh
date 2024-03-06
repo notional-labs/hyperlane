8cb58191d701172eced62bb333274a61a58fdcb4
kienn6034 <kien@notional.ventures>
2024-03-06 15:45:40 +0700

    val: run validator

--- hypr_testnet/scripts/run-validator.sh
+++ hypr_testnet/scripts/run-validator.sh
@@ -1,2 +1,22 @@
 CONFIG_FILES="${1:-./artifacts/agent-config-2024-03-06-15-19-00.json}"
 
+mkdir tmp 
+
+# Pick an informative name specific to the chain you're validating
+VALIDATOR_SIGNATURES_DIR=tmp/hyperlane-validator-signatures-hyprtestnet
+VALIDATOR_KEY=${2:-$(cat ./.keys/valkey)}
+
+echo "Using validator key: $VALIDATOR_KEY"
+
+exit 0
+# Create the directory
+mkdir -p $VALIDATOR_SIGNATURES_DIR
+
+
+cd hyperlane-monorepo
+cargo run --release --bin validator -- \
+    --db ./hyperlane_db_validator_hyprtestnet \
+    --originChainName sepolia \
+    --checkpointSyncer.type localStorage \
+    --checkpointSyncer.path $VALIDATOR_SIGNATURES_DIR \
+    --validator.key $VALIDATOR_KEY

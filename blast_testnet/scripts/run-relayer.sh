f38bd4db7bdc005568f493c6fe28c1434194b694
Duong Minh Ngoc <ngoc@notional.ventures>
2024-03-06 20:16:29 +0700

    feat: run relayer and validator blast testnet

---
+++ blast_testnet/scripts/run-relayer.sh
@@ -0,0 +1,23 @@
+HOME_DIR=$(pwd)
+export CONFIG_FILES="${1:-$HOME_DIR/blast_testnet/artifacts/agent-config-2024-03-06-16-58-34.json}"
+echo "Running relayer with config files: $CONFIG_FILES"
+
+RELAYER_KEY=${2:-$(cat $HOME_DIR/.keys/relayerkey)}
+if [ -z $RELAYER_KEY ]; then
+    echo "No validator key provided"
+    exit 1
+fi
+
+
+# Get binary folder 
+BINARY_DIR=$HOME_DIR/hyperlane-monorepo/rust
+cd $BINARY_DIR
+
+BINARY_FILE=$BINARY_DIR/target/release/relayer
+
+CONFIG_FILES=$CONFIG_FILES $BINARY_FILE \
+    --db ./hyperlane_db_relayer \
+    --relayChains blasttestnet,sepolia \
+    --allowLocalCheckpointSyncers true \
+    --defaultSigner.key $RELAYER_KEY \
+    --metrics-port 9091 \
---
+++ blast_testnet/scripts/run-validator.sh
@@ -0,0 +1,29 @@
+HOME_DIR=$(pwd)
+CONFIG_FILES="${1:-$HOME_DIR/blast_testnet/artifacts/agent-config-2024-03-06-16-58-34.json}"
+echo "Using config files: $CONFIG_FILES"
+
+# Pick an informative name specific to the chain you're validating
+mkdir -p $HOME_DIR/tmp
+VALIDATOR_SIGNATURES_DIR=$HOME_DIR/tmp/hyperlane-validator-signatures-blasttestnet
+
+BINARY_DIR=$HOME_DIR/hyperlane-monorepo/rust
+cd $BINARY_DIR
+
+BINARY_FILE=$BINARY_DIR/target/release/validator
+
+
+VALIDATOR_KEY=${2:-$(cat $HOME_DIR/.keys/valkey)}
+if [ -z $VALIDATOR_KEY ]; then
+    echo "No validator key provided"
+    exit 1
+fi
+
+# Create the directory
+mkdir -p $VALIDATOR_SIGNATURES_DIR
+
+CONFIG_FILES=$CONFIG_FILES $BINARY_FILE \
+    --db ./hyperlane_db_validator_blasttestnet \
+    --originChainName sepolia \
+    --checkpointSyncer.type localStorage \
+    --checkpointSyncer.path $VALIDATOR_SIGNATURES_DIR \
+    --validator.key $VALIDATOR_KEY

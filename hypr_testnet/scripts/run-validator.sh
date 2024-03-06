a72424d6ca3279c8ac656b4660dfac9c8542767b
kienn6034 <kien@notional.ventures>
2024-03-06 15:50:04 +0700

    feat: setup keys

--- hypr_testnet/scripts/run-validator.sh
+++ hypr_testnet/scripts/run-validator.sh
@@ -1,17 +1,18 @@
 CONFIG_FILES="${1:-./artifacts/agent-config-2024-03-06-15-19-00.json}"
-
-mkdir tmp 
+HOME_DIR=$(pwd)
 
 # Pick an informative name specific to the chain you're validating
-VALIDATOR_SIGNATURES_DIR=tmp/hyperlane-validator-signatures-hyprtestnet
-VALIDATOR_KEY=${2:-$(cat ./.keys/valkey)}
+mkdir -p $HOME_DIR/tmp
+VALIDATOR_SIGNATURES_DIR=$HOME_DIR/tmp/hyperlane-validator-signatures-hyprtestnet
 
-echo "Using validator key: $VALIDATOR_KEY"
+VALIDATOR_KEY=${2:-$(cat $HOME_DIR/.keys/valkey)}
+if [ -z $VALIDATOR_KEY ]; then
+    echo "No validator key provided"
+    exit 1
+fi
 
-exit 0
 # Create the directory
 mkdir -p $VALIDATOR_SIGNATURES_DIR
-
 
 cd hyperlane-monorepo
 cargo run --release --bin validator -- \

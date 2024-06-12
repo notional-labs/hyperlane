b1ee017f45e1528ad5bd4e742571cb00d7c6cece
minhngoc274 <ngoc@notional.ventures>
2024-06-12 08:29:49 +0700

    feat: add hyperliquid testnet config

---
+++ hyperliquid/hyperliquid-testnet/scripts/deploy-warp.sh
@@ -0,0 +1,16 @@
+HOME_DIR=$(pwd)
+echo $HOME_DIR
+export WARP_CONFIG_FILE="${1:-$HOME_DIR/configs/warp-route-deployment.yaml}"
+BASE_DIR=$(realpath "$(dirname "$(realpath "$0")")/../../../")
+export REGISTRY="${1:-$BASE_DIR/hyperlane-registry}"
+
+DEPLOYER_KEY=${2:-$(cat $HOME_DIR/.keys/deployerkey)}
+if [ -z $DEPLOYER_KEY ]; then
+    echo "No deployer key provided"
+    exit 1
+fi
+
+hyperlane deploy warp \
+    --registry "$REGISTRY" \
+    --config "$WARP_CONFIG_FILE"\
+    --key "$DEPLOYER_KEY"

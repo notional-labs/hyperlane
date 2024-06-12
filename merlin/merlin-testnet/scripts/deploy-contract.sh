32b0e9f3430a6cf1d02d0d0bb6c68ce114dc56fd
minhngoc274 <ngoc@notional.ventures>
2024-06-12 08:28:41 +0700

    feat: add merlin testnet config

---
+++ merlin/merlin-testnet/scripts/deploy-contract.sh
@@ -0,0 +1,17 @@
+HOME_DIR=$(pwd)
+echo $HOME_DIR
+export MULTISIG_CONFIG_FILE="${1:-$HOME_DIR/configs/ism.yaml}"
+BASE_DIR=$(realpath "$(dirname "$(realpath "$0")")/../../../")
+export REGISTRY="${1:-$BASE_DIR/hyperlane-registry}"
+
+DEPLOYER_KEY=${2:-$(cat $HOME_DIR/.keys/deployerkey)}
+if [ -z $DEPLOYER_KEY ]; then
+    echo "No deployer key provided"
+    exit 1
+fi
+
+hyperlane deploy core \
+    --registry "$REGISTRY" \
+    --targets merlintestnet,sepolia \
+    --ism "$MULTISIG_CONFIG_FILE" \
+    --key "$DEPLOYER_KEY"
